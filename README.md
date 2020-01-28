a2b47a5aac0d9b12fa8078c359c214c2bfa5651a
romanowski <romanowski@10.81.149.197>
2020-01-28 11:44:11 -0500

    Base

---
+++ README.md
@@ -0,0 +1,21 @@
+# Project Title
+
+Arista Deployment Builder Script UI
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+```
+pip install xlrd
+pip install xlwt
+pip install cherrypy
+```
+
+### Installing
+
+```
+Give the example
+```
--- server.py
+++ server.py
@@ -1,8 +1,96 @@
 import cherrypy
+import xlrd
+import xlwt
+import json
+import os
 
-class HelloWorld(object):
+localDir = os.path.dirname(__file__)
+absDir = os.path.join(os.getcwd(), localDir)
+
+class Handler(object):
     @cherrypy.expose
     def index(self):
-        return "Hello World!"
+        f = open("index.html", "r")
+        
+        return f.read()
+    
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    @cherrypy.tools.json_in()
+    def run(self):
 
-cherrypy.quickstart(HelloWorld())
+        result = {"operation": "request", "result": "success"}
+        
+        input_json = cherrypy.request.json
+        print(input_json)
+        return result
+    
+    @cherrypy.expose
+    def readfile(self):
+        with xlrd.open_workbook('workbook.xls') as f:
+            
+            
+            
+            toReturn = {}
+            def format(v):
+                if type(v) == float:
+                    return {'type':'text','title':int(val), 'width':200 }
+                else:
+                    return {'type':'text','title':v.lower(), 'width':200 }
+                
+            
+
+            for n in range(0, f.nsheets):
+                _sheet=f.sheet_by_index(n)
+                _sheet.cell_value(0,0)
+                toReturn[_sheet.name] = {'data':[],'columns':[format(val) for val in _sheet.row_values(0)]}
+                for row in range(1, _sheet.nrows):
+                    row = _sheet.row_values(row)
+                    toReturn[_sheet.name]['data'].append(row)
+                
+        return json.dumps(toReturn)
+    
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    @cherrypy.tools.json_in()
+    def writefile(self):
+
+        result = {"operation": "request", "result": "success"}
+        wb = xlwt.Workbook()
+        input_json = cherrypy.request.json
+        for sheet in input_json:
+            tab = sheet[0]
+            data = sheet[1:]
+            
+            ws = wb.add_sheet(tab)
+            for r, row in enumerate(data):
+                for c, v in enumerate(row):
+                    ws.write(r,c,v)
+        
+        wb.save('workbook.xls')
+        # Responses are serialized to JSON (because of the json_out decorator)
+        return result
+    
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def upload(self, myFile):
+
+        result = {"operation": "request", "result": "success"}
+
+        size = 0
+        f = open("workbook.xls", "wb")
+        
+        
+        while True:
+            data = myFile.file.read(8192)
+            f.write(data)
+            if not data:
+                f.close()
+                break
+
+        # Responses are serialized to JSON (because of the json_out decorator)
+        return result
+    
+
+
+cherrypy.quickstart(Handler(),'/','handler.conf')
